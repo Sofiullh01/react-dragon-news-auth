f9b03fbc38dae4caf7e8d2c9f7753cb4bdd5fd78
Sofiullah01 <ssisofiulislam@gmail.com>
2023-10-04 04:33:30 +0600

    added login rroute

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>login here</h2>
+        </div>
+    );
+};
+
+export default Login;
